1621063d0345349a459847e7b89324f6c42921ae
Pieterjan Van Landeghem <pieterjan.vanlandeghem@student.hogent.be>
2022-12-08 01:30:46 +0100

    pages testen

---
+++ 5_data_analyseren.py
@@ -0,0 +1,42 @@
+#! /usr/bin/python
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sys
+import pandas as pd
+#Finds the csv file
+DIR = sys.argv[1]
+TIME = 'analyse/'+sys.argv[2]
+CSV_FILE = DIR + "/data.csv"
+print(CSV_FILE)
+
+df = pd.read_csv(CSV_FILE).drop_duplicates()
+print(df)
+
+#Datum omzetten:
+df['time'] = pd.to_datetime(df['time'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%d/%m/%y %H:%M:%S')
+print(df)
+
+#Algemene plot maken dat graden en bezetting bekijkt:
+eerste_plot = df.pivot_table(index='graden', columns='facilityName', values='bezetting%', aggfunc='mean')
+print(eerste_plot)
+eerste_plot.plot(title='Invloed temperatuur bezetting fietsenstalling', xlabel='graden celsius', ylabel='bezetting in %')
+plt.savefig(str(TIME)+'/grafiek1')
+
+#Algemene plot maken dat datum en bezetting bekijkt:
+tweede_plot = df.pivot_table(index='time', columns='facilityName', values='bezetting%', aggfunc='mean')
+print(tweede_plot)
+tweede_plot.plot(title='Tijdsaanduiding wanneer data is opgenomen en hun bezetting', xlabel='datum', ylabel='bezetting in %')
+plt.savefig(str(TIME)+'/grafiek2')
+
+#Mean plot maken dat graden en bezetting bekijkt:
+derde_plot = df.pivot_table(index='time',values='bezetting%', aggfunc='mean')
+print(derde_plot)
+derde_plot.plot(title='Gemiddelde tijdsaanduiding wanneer data is opgenomen en hun bezetting', xlabel='datum', ylabel='bezetting in %', legend='test')
+plt.savefig(str(TIME)+'/grafiek3')
+
+#Algemene plot maken dat datum en bezetting bekijkt:
+derde_plot = df.pivot_table(index='graden',values='bezetting%', aggfunc='mean')
+print(derde_plot)
+derde_plot.plot(title='Gemiddelde tijdsaanduiding wanneer data is opgenomen en hun bezetting', xlabel='graden', ylabel='bezetting in %')
+plt.savefig(str(TIME)+'/grafiek4')
---
+++ README.md
@@ -0,0 +1,2 @@
+
+Reference to another [document in a subfolder](rapport/2022-12-07_23:39:40.md).
