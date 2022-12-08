e1eecf8674bfe119b97eb652774fecf93141f078
PieterjanVL <60927400+PieterjanVL@users.noreply.github.com>
2022-12-08 18:52:27 +0100

    Vanuit linux env

--- 1_main.sh
+++ 1_main.sh
@@ -3,7 +3,7 @@ set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
-DIR=$HOME/Documents/Rapport
+DIR=/home/pieterjan/Documents/Hogent/22-23/Linux/Rapport
 timestamp=$(date +%F_%T)
 timestamp2=$(date +%F_%H-%M-%S)
 
@@ -13,12 +13,9 @@ echo "${timestamp2}"
 export DIR
 export timestamp
 export timestamp2
-$HOME/Documents/Rapport/2_setup.sh
-$HOME/Documents/Rapport/3_data_verzamelen.sh
-$HOME/Documents/Rapport/4_data_transformeren.sh
-python3 $HOME/Documents/Rapport/5_data_analyseren.py "${DIR}" "${timestamp2}"
-python3 $HOME/Documents/Rapport/6_raport_genereren.py "${DIR}" "${timestamp2}"
+/home/pieterjan/Documents/Hogent/22-23/Linux/Rapport/2_setup.sh
+/home/pieterjan/Documents/Hogent/22-23/Linux/Rapport/3_data_verzamelen.sh
+/home/pieterjan/Documents/Hogent/22-23/Linux/Rapport/4_data_transformeren.sh
+python3 /home/pieterjan/Documents/Hogent/22-23/Linux/Rapport/5_data_analyseren.py "${DIR}" "${timestamp2}"
+python3 /home/pieterjan/Documents/Hogent/22-23/Linux/Rapport/6_raport_genereren.py "${DIR}" "${timestamp2}"
 printf "* Rapport gemaakt op [%s](rapport/%s.md)\n" "${timestamp}" "${timestamp2}" | cat - README.md > temp && mv temp README.md
-#./2_setup.sh 
-#./3_data_verzamelen.sh #STAP 1
-#./4_data_transformeren.sh #STAP 2
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-* Rapport gemaakt op [2022-12-08_18:31:41](rapport/2022-12-08_18-31-41.md)
-* Rapport gemaakt op [2022-12-08_18:28:04](rapport/2022-12-08_18-28-04.md)
-* Rapport gemaakt op [2022-12-08_18:27:11](rapport/2022-12-08_18-27-11.md)
+* Rapport gemaakt op [2022-12-08_18:49:58](rapport/2022-12-08_18-49-58.md)
+* Rapport gemaakt op [2022-12-08_18:49:42](rapport/2022-12-08_18-49-42.md)
+* Rapport gemaakt op [2022-12-08_18:48:34](rapport/2022-12-08_18-48-34.md)
+
 
