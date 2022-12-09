23ffa32611fd61b26b53ab44d621f8e08a1f04f5
Pieterjan Van Landeghem <pieterjan.vanlandeghem@student.hogent.be>
2022-12-09 17:46:31 +0100

    aanmaken folder github actions

--- 7_test.py
+++ 7_test.py
@@ -8,24 +8,13 @@ import pandas as pd
 df = pd.read_csv(os.getcwd() + '/data.csv').drop_duplicates()
 print(df)
 
-#Datum omzetten:
-df['time'] = pd.to_datetime(df['time'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%d/%m/%y %H:%M:%S')
-print(df)
-
-#Algemene plot maken dat graden en bezetting bekijkt:
-eerste_plot = df.pivot_table(index='graden', columns='facilityName', values='bezetting%', aggfunc='mean')
-print(eerste_plot)
-eerste_plot.plot(title='Invloed temperatuur bezetting fietsenstalling', xlabel='graden celsius', ylabel='bezetting in %')
-plt.savefig(os.getcwd()+'/grafiek1')
-
-
-print(os.path.exists(os.getcwd() + '/grafiek1'))
+print(os.path.exists(os.getcwd()))
 print('File is gemaakt' + os.getcwd())
 
 print(os.path.exists(os.getcwd() + '/analyse/2022-12-08_18-48-34'))
 print('File is gemaakt' + os.getcwd() + '/analyse')
 
-os.makedirs(os.getcwd() + '/analyse/test')
+os.makedirs(os.getcwd() + '/analyse/test4')
 
-print(os.path.exists(os.getcwd() + '/analyse/test'))
-print('File is gemaakt' + os.getcwd() + '/analyse/test')
+print(os.path.exists(os.getcwd() + '/analyse/test4'))
+print('File is gemaakt' + os.getcwd() + '/analyse/test4')
