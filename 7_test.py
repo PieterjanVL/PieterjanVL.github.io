c8cd039fda53cc8d3b20721af13bbc727c738819
Pieterjan Van Landeghem <pieterjan.vanlandeghem@student.hogent.be>
2022-12-09 17:17:02 +0100

    aanmaken folder github actions

--- 7_test.py
+++ 7_test.py
@@ -2,5 +2,5 @@ import os
 from os.path import abspath, join, dirname
 PROJECT_ROOT_PATH = abspath(join(dirname(abspath(__file__)), '..'))
 
-print(os.path.exists(PROJECT_ROOT_PATH + 'analyse'))
-print('File is gemaakt  ' + PROJECT_ROOT_PATH)
+print(os.path.exists(PROJECT_ROOT_PATH + '/analyse'))
+print('File is gemaakt  ' + PROJECT_ROOT_PATH + '/analyse')
