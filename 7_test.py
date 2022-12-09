f68231aedc8afe3b0eaef841bd339fa4de50287c
Pieterjan Van Landeghem <pieterjan.vanlandeghem@student.hogent.be>
2022-12-09 17:02:32 +0100

    aanmaken folder github actions

--- 7_test.py
+++ 7_test.py
@@ -1,9 +1,6 @@
 import os
 
-from os.path import abspath, join, dirname
-PROJECT_ROOT_PATH = abspath(join(dirname(abspath(__file__)), '..'))
-print(PROJECT_ROOT_PATH)
-parent_dir = PROJECT_ROOT_PATH + '/analyse'
+parent_dir = os.getcwd() + '/analyse'
 
 directory = 'test'
 
