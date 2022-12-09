b7887046db429b58466b609b5900c96d204919d4
Pieterjan Van Landeghem <pieterjan.vanlandeghem@student.hogent.be>
2022-12-10 00:27:57 +0100

    aanmaken folder github actions

--- 7_test.py
+++ 7_test.py
@@ -8,7 +8,9 @@ from datetime import datetime
 
 now = datetime.now()
 
-dt_string = now.strftime("%Y-%m-%d %H_%M_%S")
+dt_string = now.strftime("%Y-%m-%d_%H-%M-%S")
+
+save_grafiek = os.getcwd()+'/analyse/' + str(dt_string)
 
 print(os.path.exists(os.getcwd()))
 print('File is gemaakt' + os.getcwd())
@@ -20,7 +22,36 @@ os.makedirs(os.getcwd() + '/analyse/' + dt_string)
 
 print(os.path.exists(os.getcwd() + '/analyse/' + dt_string))
 
+df = pd.read_csv(os.getcwd() + '/data.csv').drop_duplicates()
+print(df)
 
+#Datum omzetten:
+df['time'] = pd.to_datetime(df['time'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%d/%m/%y %H:%M:%S')
+print(df)
+
+#Algemene plot maken dat graden en bezetting bekijkt:
+eerste_plot = df.pivot_table(index='graden', columns='facilityName', values='bezetting%', aggfunc='mean')
+print(eerste_plot)
+eerste_plot.plot(title='Invloed temperatuur bezetting fietsenstalling', xlabel='graden celsius', ylabel='bezetting in %')
+plt.savefig(save_grafiek+'/grafiek1')
+
+#Algemene plot maken dat datum en bezetting bekijkt:
+tweede_plot = df.pivot_table(index='time', columns='facilityName', values='bezetting%', aggfunc='mean')
+print(tweede_plot)
+tweede_plot.plot(title='Tijdsaanduiding wanneer data is opgenomen en hun bezetting', xlabel='datum', ylabel='bezetting in %')
+plt.savefig(save_grafiek+'/grafiek2')
+
+#Mean plot maken dat graden en bezetting bekijkt:
+derde_plot = df.pivot_table(index='time',values='bezetting%', aggfunc='mean')
+print(derde_plot)
+derde_plot.plot(title='Gemiddelde tijdsaanduiding wanneer data is opgenomen en hun bezetting', xlabel='datum', ylabel='bezetting in %', legend='test')
+plt.savefig(save_grafiek+'/grafiek3')
+
+#Algemene plot maken dat datum en bezetting bekijkt:
+derde_plot = df.pivot_table(index='graden',values='bezetting%', aggfunc='mean')
+print(derde_plot)
+derde_plot.plot(title='Gemiddelde tijdsaanduiding wanneer data is opgenomen en hun bezetting', xlabel='graden', ylabel='bezetting in %')
+plt.savefig(save_grafiek+'/grafiek4')
 
 f= open(os.getcwd() + '/analyse/' + dt_string + '/ok.txt',"w+")
 for i in range(60):
