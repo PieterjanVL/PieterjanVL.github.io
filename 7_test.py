f0373f422b9145d90d6292eb2077367661b8ef09
Pieterjan Van Landeghem <pieterjan.vanlandeghem@student.hogent.be>
2022-12-09 19:21:01 +0100

    aanmaken folder github actions

--- 7_test.py
+++ 7_test.py
@@ -22,5 +22,5 @@ print(os.path.exists(os.getcwd() + '/analyse/test6/test8'))
 print('File is gemaakt' + os.getcwd() + '/analyse/test6/test8')
 
 f= open(os.getcwd() + '/analyse/ok/ok.txt',"w+")
-for i in range(20):
+for i in range(60):
      f.write("This is line %d\r\n" % (i+1))
