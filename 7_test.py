5c92b74c17288c081fa7e2c0cb518e401cdbd775
Pieterjan Van Landeghem <pieterjan.vanlandeghem@student.hogent.be>
2022-12-09 17:34:21 +0100

    aanmaken folder github actions

--- 7_test.py
+++ 7_test.py
@@ -3,7 +3,7 @@ import os
 print(os.path.exists(os.getcwd()))
 print('File is gemaakt' + os.getcwd())
 
-print(os.path.exists(os.getcwd() + '/analyse'))
+print(os.path.exists(os.getcwd() + '/analyse/2022-12-08_18-48-34'))
 print('File is gemaakt' + os.getcwd() + '/analyse')
 
 os.makedirs(os.getcwd() + '/analyse/test')
