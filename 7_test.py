ddc0e8393b50ded3974960da085aee135ee9c62c
Pieterjan Van Landeghem <pieterjan.vanlandeghem@student.hogent.be>
2022-12-09 17:40:46 +0100

    aanmaken folder github actions

--- 7_test.py
+++ 7_test.py
@@ -1,5 +1,22 @@
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sys
+import pandas as pd
 
+df = pd.read_csv(os.getcwd() + '/data.csv').drop_duplicates()
+print(df)
+
+#Datum omzetten:
+df['time'] = pd.to_datetime(df['time'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%d/%m/%y %H:%M:%S')
+print(df)
+
+#Algemene plot maken dat graden en bezetting bekijkt:
+eerste_plot = df.pivot_table(index='graden', columns='facilityName', values='bezetting%', aggfunc='mean')
+print(eerste_plot)
+eerste_plot.plot(title='Invloed temperatuur bezetting fietsenstalling', xlabel='graden celsius', ylabel='bezetting in %')
+plt.savefig(os.getcwd()+'/grafiek1')
 print(os.path.exists(os.getcwd()))
 print('File is gemaakt' + os.getcwd())
 
